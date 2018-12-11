cecceb268f2991faa906ef0dddd43f5c431adb88
平田 修一 <Shuichi@hirata-no-MacBook-Air.local>
2018-12-11 17:37:21 +0900

    meshiterro

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '1d2a693434ff0018b69aeb9ef899afca7367168638bec79b3cf1f0ad280b65d9b4b2b243470a20ba9a9bd4243718aeaee33b0f23db84d6e9576fbdb169e26bd5'
